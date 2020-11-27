db6e13a2cc1ffa26d5dc75d96b421cb0d7e9ab82
Melweezy <71882265+Melweezy@users.noreply.github.com>
2020-11-27 02:11:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var counter=0;
+
+document.addEventListener('keyup', event => {
+    if (event.code === 'Space') {
+      if(character.classList == "jump"){return}
+    character.classList.add("jump");
+    setTimeout(function(){
+        character.classList.remove("jump");
+    },300);
+      }
+});
+document.addEventListener('keyup', event => {
+    if (event.code === 'KeyD') {
+        let characterLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+        characterLeft+10;
+}});
+// document.addEventListener('keyup', event => {
+//     if (event.code === "KeyA") {
+//       if(character.classList == "animate"){return}
+//     character.classList.add("animate");
+//     setTimeout(function(){
+//         character.classList.remove("animate");
+//     },300);
+//       }
+// });
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<145 && blockLeft> 60 && characterTop>=420){
+        block.style.animation = "none";
+        alert("Game Over. score: "+Math.floor(counter/50));
+        counter=0;
+        block.style.animation = "block 2s infinite linear";
+    }else {
+        counter++;
+        document.getElementById("score").innerHTML = Math.floor(counter/50);
+    }
+}, 10);
